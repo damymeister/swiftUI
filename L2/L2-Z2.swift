f2b1b7bb4d7b307b428b8adc2aa5d7f09619b587
damymeister <77884237+damymeister@users.noreply.github.com>
2023-03-06 13:25:28 +0100

    Update L2-Z2.swift

--- L2/L2-Z2.swift
+++ L2/L2-Z2.swift
@@ -1,1 +1,30 @@
 
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("nazwa-zdjecia")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Tytuł zdjęcia")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                    Text("Autor zdjęcia")
+                        .font(.subheadline)
+                        .foregroundColor(.primary)
+                }
+                Spacer()
+            }
+            .padding()
+            .background(Color.primary.opacity(0.8))
+            .cornerRadius(10)
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+    }
+}
