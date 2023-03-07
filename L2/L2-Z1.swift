30dcc915390de856bf0397033e4684222ebc8e28
damymeister <77884237+damymeister@users.noreply.github.com>
2023-03-07 09:32:16 +0100

    Update L2-Z1.swift

--- L2/L2-Z1.swift
+++ L2/L2-Z1.swift
@@ -4,18 +4,17 @@ struct ContentView: View {
     let tablica_kolorow = [Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.purple]
     
     var body: some View {
-        VStack {
-            ForEach(0..<tablica_kolorow.count) { i in
+        ZStack {
+            ForEach(0..<tablica_kolorow.count) {
                 Rectangle()
-                    .fill(tablica_kolorow[i])
+                    .fill(tablica_kolorow[$0])
                     .frame(width: 200, height: 200)
-                    .offset(x: CGFloat(index) * 210, y: 0)
+                    .offset(x: CGFloat($0) * 10.0,y: CGFloat($0) * 10.0)
             }
         }
         .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
