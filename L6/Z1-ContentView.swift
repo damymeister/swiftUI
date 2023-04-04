55859dedd0b3654cd37557f53252e37b3c734081
damymeister <77884237+damymeister@users.noreply.github.com>
2023-04-04 09:34:48 +0200

    Create Z1-ContentView.swift

---
+++ L6/Z1-ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        FirstView()
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
