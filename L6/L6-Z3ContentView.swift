e0c5f1c50cccfee1d908d4f1b58acab20f724d37
damymeister <77884237+damymeister@users.noreply.github.com>
2023-04-15 22:46:45 +0200

    Create L6-Z3ContentView.swift

---
+++ L6/L6-Z3ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+struct ContentView: View{
+
+    var body: some View{
+
+        FirstView()
+
+    }
+
+}
+struct ContentView_Previews: PreviewProvider{
+
+    static var previews: some View{
+
+        ContentView()
+    }
+}
