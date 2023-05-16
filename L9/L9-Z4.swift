8b4e03478c4bfb1a6ea221f2b3e1fe9bb9dafd5e
damymeister <77884237+damymeister@users.noreply.github.com>
2023-05-16 09:22:46 +0200

    Create L9-Z4.swift

---
+++ L9/L9-Z4.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+struct Car{
+    var brand: String
+    var model: String
+    var logo: Image
+}
+struct ContentView: View {
+    @State var auta: [Car] = [
+        Car(brand:"Lamborghini", model: "Hurracan", logo: Image("lambo")),
+        Car(brand:"Ferrari", model:"Enzo", logo:Image("ferrari")),
+        Car(brand:"Bugatti", model:"Chiron", logo: Image("bugatti"))
+    ]
+    var body: some View {
+        VStack {
+            List{
+                ForEach(0..<auta.count){
+                    a in CarView(auto: $auta[a])
+                        .gesture(TapGesture()
+                            .onEnded(){
+                                auta.remove(at: a)
+                            })
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+import SwiftUI
+
+struct CarView: View {
+    @Binding var auto: Car
+    var body: some View {
+        HStack{
+            Text("\(auto.brand) ")
+            Text("\(auto.model) ")
+            auto.logo
+                .resizable()
+                .frame(width:40, height:30)
+        }
+    }
+}
+
+struct CarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarView(auto:. constant(Car(brand: "", model: "", logo:Image(""))))
+    }
+}
